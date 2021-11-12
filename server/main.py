dd178f860efcc8c074aab64a4b56cb7aaa3925bd
sir-timio <t-ionov@list.ru>
2021-11-13 01:49:27 +0300

    update

--- server/main.py
+++ server/main.py
@@ -7,12 +7,6 @@ import uvicorn
 
 app = FastAPI()
 
-
-
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
 # example = http://127.0.0.1:8000/stats/1
 # example with optional param http://127.0.0.1:8000/stats/1?q=1
 
@@ -27,10 +21,6 @@ def get_stats(id, q=None):
         'values': values
     })
     return d
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
 
 if __name__ == '__main__':
     uvicorn.run(app)
